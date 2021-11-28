5b67afe0c3bf496c3d3bef21f82d68f4b447f850
Éverton Costa de Araújo <earaujo-cit@github.com>
2021-11-28 20:45:11 -0300

    Atividade Exploraçao Espacial

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,11 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const constante = document.querySelector('#constante')
+let massa1El = document.querySelector('#massa1')
+let massa2El = document.querySelector('#massa2')
+let distanciaEl = document.querySelector('#distancia')
+let forcaGEl = document.querySelector('#resultado')
+let calcularEl = document.querySelector('#calcular')
+calcularEl.addEventListener('click', function() {
+    forcaGEl.value = constante.value * massa1El.value * massa2El.value / (Math.pow(distanciaEl.value, 2))
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,12 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+let paragrafos = document.querySelectorAll('p > button')
+for (let pEl of paragrafos) {
+    pEl.addEventListener('click', expande)
+}
+
+function expande(e) {
+    let el = e.target;
+    el.parentElement.classList.toggle('expandido')
+    console.log('clicado')
+}
